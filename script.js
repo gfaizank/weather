e68ebd71deeb2ddd4efcf9fefed956e379eddaba
gfaizank <114975001+gfaizank@users.noreply.github.com>
2023-08-03 20:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const button=document.getElementById("search-button");
+const input=document.getElementById("city-input");
+
+const cityName= document.getElementById('city-name');
+const cityTime= document.getElementById('city-time');
+const cityTemp= document.getElementById('city-temp');
+
+
+
+
+async function getData(cityName){
+    const promise= await fetch(`http://api.weatherapi.com/v1/current.json?key=f8db4d58b33e4778ae4141440232807&q=${cityName}&aqi=yes`);
+    return await promise.json();
+}
+
+button.addEventListener('click', async () => {
+    const value= input.value;
+    const result= await getData(value);
+    console.log(result);
+    cityName.innerText= `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    cityTime.innerText= result.location.localtime;
+    cityTemp.innerText=result.current.temp_c;
+})
+
+// http://api.weatherapi.com/v1/current.json?key=f8db4d58b33e4778ae4141440232807&q=London&aqi=yes
